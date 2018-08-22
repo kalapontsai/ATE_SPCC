fdcf16c4a661db7ef3cfb2dd19f0a483d2fa49bc
kalapontsai <kadelat@gmail.com>
2018-08-22 11:32:47 +0800

    combine in one file and add debug code for shell

--- stat_single_dir.py
+++ stat_single_dir.py
@@ -10,31 +10,46 @@
 #os.path.splitext() 分離檔案名稱與副檔名
 #array_name = [[0 for i in range(m)] for j in range(n)] 建立m*n的二維陣列
 
-import os.path
-import stat_single_file as sf
+import os
+from win32com.client import Dispatch, constants
 
-listdir = []
-listfile = []
+def get_yield( filepath = "d:\\kadela\\git\\ATE_SPCC\\sample\\", filename = "YS11-17110027.csv",sampling = 0):
+	#filepath = "d:\\kadela\\git\\ATE_SPCC\\sample\\" #區域網路要先設網路磁碟機代號
+	#filename = "YS11-17110027.csv"
+	xlsfile = os.path.join(filepath,filename)
+	xlsApp = Dispatch("Excel.Application")
+	xlsApp.Visible = 0                  #顯示 Excel
+	xlsBook = xlsApp.Workbooks.open(xlsfile)    #開啟一工作簿
+	lot_id = os.path.splitext(filename)[0]      #分離前檔名及副檔名
+	#print ("sheetname :", lot_id)
+	xlsSheet = xlsBook.Worksheets(lot_id)  
 
-curr_dir = "d:\\kadela\\git\\ATE_SPCC\\sample\\"
-tmp_list = os.listdir(curr_dir)
-for i in tmp_list:
-	#tmp_i = os.path.join(curr_dir,i)
-	if os.path.isdir(os.path.join(curr_dir,i)):
-		listdir.append(i)
-	else:
-		listfile.append(i)
-#print (listdir,listfile) #列出所有目錄跟檔名
+	row = 16   #列
+	col = 2 #欄
+	total = 0
+	good = 0
+	#result = []
+	answer = ['PASS','ABORT','FAIL']
+	if sampling == 0 : sampling = 999999
+	while (str.strip(xlsSheet.Cells(row,col).Value) in answer) and (total < sampling): #xls表格內有多餘的空格
+		if str.strip(xlsSheet.Cells(row,col).Value) == 'PASS' :  
+			good += 1
+		row += 1
+		total += 1
+		if type(xlsSheet.Cells(row,col).Value) == type(None):
+			break  # 偵測下一個若是空白則結束
+	xlsBook.Close()
+	xlsApp.Quit()                #結束 Excel
+	del xlsApp
+	return (lot_id,total,good)
 
 yield_data = []
-for i in listfile:
-	t = sf.get_yield(filepath = curr_dir, filename = i, sampling = 30)
-	#print ('---------')
-	#print ('lotid:',t[0])
-	#print ('total:',t[1])
-	#print ('good :',t[2])
-	yield_data += [t[0],t[1],t[2]]
-
-print(yield_data)
-
-
+curr_dir = "d:\\kadela\\git\\ATE_SPCC\\sample_2\\"
+for Path, Folder, FileName in os.walk(curr_dir):
+	for i in FileName:
+		tmp = Path + "\\" +i
+		if i[:2] == "YS" and i[-3:] == "csv" :
+			#print (Path,i)
+			t = get_yield(filepath = Path, filename = i, sampling = 10)
+			yield_data += [t[0],t[1],t[2]]
+print (yield_data)
