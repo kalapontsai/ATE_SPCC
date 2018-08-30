6975b22f38513fc924ab37de992f259d9809d0ec
kalapontsai <kadelat@gmail.com>
2018-08-30 10:22:02 +0800

    add log file in save_into_sqlite3.py

--- save_into_sqlite3.py
+++ save_into_sqlite3.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
 #對指定目錄內所有測試檔進行統計, 紀錄[檔案修改時間], [lot id], [總數], [良品數]
-#再寫入新的csv檔
+#再寫入Django的資料庫,以及log紀錄
 
 import os,time
 from datetime import datetime, timezone
 from win32com.client import Dispatch, constants
 import sqlite3
+import csv
 
 def get_yield( filepath = "d:\\kadela\\git\\ATE_SPCC\\sample\\", filename = "YS11-17110027.csv",sampling = 0):
 	xlsfile = os.path.join(filepath,filename)
@@ -30,17 +32,23 @@ def get_yield( filepath = "d:\\kadela\\git\\ATE_SPCC\\sample\\", filename = "YS1
 	xlsApp.Quit()
 	del xlsApp
 	return (lot_id,total,good)
-
+#Django資料庫路徑
 conn = sqlite3.connect('d:\\kadela\\git\\mysite\\db.sqlite3')
+#測試資料存檔路徑
 curr_dir = "d:\\kadela\\git\\ATE_SPCC\\sample_2\\"
 c = conn.cursor()
+
+#設定log的路徑與檔名
+dt = str(datetime.now().strftime("%Y%m%d%H%M%S"))
+log_file = "d:\\kadela\\git\\ATE_SPCC\\log\\atelog-" + dt + ".csv"
+f = open(log_file, 'w', newline='')
+w = csv.writer(f)
 
 for Path, Folder, FileName in os.walk(curr_dir):
 	for i in FileName:
 		tmp = Path + "\\" +i
 		if i[:2] == "YS" and i[-3:] == "csv" :
 			m_time = os.path.getmtime(os.path.join(Path,i))
-			#f_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(m_time))
 			f_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(m_time))
 			print (Path,i,f_time)
 			t = get_yield(filepath = Path, filename = i, sampling = 0)
@@ -49,9 +57,9 @@ for Path, Folder, FileName in os.walk(curr_dir):
 			VALUES ('" + f_time + "', '"+ t[0][5:] + "', '" + str(t[1]) + "', '" + str(t[2]) + "', '"  + str(yie) + "')"
 			print (sql)
 			c.execute(sql)
+			w.writerow([sql]) #以list放入,每個字元不會被逗號分開
 
 c.close()
 conn.commit()
 conn.close()
-#"INSERT INTO ate_spcc_test_static (create_date,lotid,total,good,yield_g) VALUES (" + m_time + ", " + str(t[0][5:]) + ", " + t[1] + ", " + t[2] + ", " + int(t[2])/int(t[1]) + ")" 
-#  "INSERT INTO ate_spcc_test_static (create_date,lotid,total,good,yield_g) VALUES('2018-08-22 11:00:21', '17110099', '5', '5', '100')"
+f.close()
